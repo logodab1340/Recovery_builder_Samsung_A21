3d6963b29551ade672d03b1fa82ed12f56b6c7b3
logodab1340 <52081679+logodab1340@users.noreply.github.com>
2024-02-22 22:22:34 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@ export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp
 export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
+export DEVICE="a21" # codename used in device tree
 export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
 export DT_BRANCH="twrp" # device tree branch
 export VENDOR="samsung" # device manufacturer or vendor
